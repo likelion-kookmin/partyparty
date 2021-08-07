34adb41f01f086625242e57a23c631d04f6952cc
soosbk <soobin851@kookmin.ac.kr>
2021-08-07 21:16:11 +0900

    일차

--- party_party/goods/urls.py
+++ party_party/goods/urls.py
@@ -5,4 +5,7 @@ from django.urls import path
 
 urlpatterns = [
     path('goods_detail/', goods_detail, name = "detail"),
+    path('write_semigoods/', semi_goods, name = "write_semigoods"),
+    path('write_goods/', goods, name = "write_goods"),
+    path('write_choices/',write_choices,name="write_choices"),
 ]
--- party_party/goods/views.py
+++ party_party/goods/views.py
@@ -4,3 +4,17 @@ from .models import Goods
 def goods_detail(request) :
     goods = Goods.object.all()
     return render(request, 'goods_detail.html')
+
+
+def write_choices(request):
+    return render(request, 'write_choices.html')
+
+def semi_goods(request) : #수요조사폼이 글작성
+    semigoods=SemiGoods.object.all()
+    return render(request, 'write_semigoods.html')
+
+
+def goods(request): #입금폼 작성
+    goods = Goods.object.all()
+    return render(request, 'write_goods.html')
+    
