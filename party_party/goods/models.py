d1b0561bb6ba6139efb678a9785b5ef1b79da859
kjsoo-1010 <tn1078@kookmin.ac.kr>
2021-08-09 17:31:59 +0900

    change repository

--- party_party/goods/models.py
+++ party_party/goods/models.py
@@ -14,25 +14,25 @@ ITEM_CHOICES= {
 # Create your models here.
 class Goods(models.Model) :
     
-    product = models.CharField(max_length = 20)
-    product_image = models.ImageField()
-    writer = models.ForeignKey(User, on_delete = CASCADE, related_name = "regist")
-    price = models.IntegerField()
-    count = models.IntegerField()
+    product = models.CharField(max_length = 20) #상품명
+    product_image = models.ImageField(blank = True) #제품 대표이미지
+    # writer = models.ForeignKey(User, on_delete = CASCADE, related_name = "regist") #작성자
+    price = models.IntegerField(null = True, blank = True, default = '') #가격
+    #count = models.IntegerField() #재고
     item = models.CharField(max_length=80, choices=ITEM_CHOICES)  #상품 분류
-    end_date=models.DateField(db_column='End Date') #마감일
-    term_needs = models.DateField()
-    term_deposit = models.IntegerField()
-    information_needs = models.CharField(max_length=500)
-    information_deposit = models.CharField(max_length=500)
-    account_deposit = models.CharField(max_length= 50)
-    count_needs = models.IntegerField()
-    count_deposit = models.IntegerField()
+    #end_date=models.DateField(db_column='End Date') #마감일
+    #term_needs = models.DateField() #수요조사폼 기간
+    #term_deposit = models.IntegerField() #입금폼 기간
+    #information_needs = models.CharField(max_length=500) #수요조사폼 info
+    #information_deposit = models.CharField(max_length=500) #입금폼 info
+    #account_deposit = models.CharField(max_length= 50) #입금계좌
+    #count_needs = models.IntegerField() #수요조사시 구매할 개수
+    #count_deposit = models.IntegerField() #입금시 구매할 개수
 
-    howto_delivery = models.CharField(max_length=10)
-    location1 = models.CharField(max_length=20)
-    location2 = models.CharField(max_length=20)
-    zip_code = IntegerField()
+    #howto_delivery = models.CharField(max_length=10) #배송방법
+    #location1 = models.CharField(max_length=20) #주소
+    #location2 = models.CharField(max_length=20) #상세주소
+    #zip_code = IntegerField() #우편번호
 
 class SemiGoods(models.Model):
 
